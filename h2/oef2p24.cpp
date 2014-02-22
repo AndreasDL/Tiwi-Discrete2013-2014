91c6da00ceb409f012c393243ff6b8e3cd17a6bf
AndreasDL <andreasdelille@hotmail;com>
2014-02-22 15:19:27 +0100

    opdeling

---
+++ h2/oef2p24.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+bool uitdrukking(const bool p, const bool q , bool r);
+bool alsdan(const bool p, const bool q);
+
+int main(int argc, char** argv){
+	cout << "uitdrukking 1" << endl;
+	for (int p = 0 ; p <= 1 ; p++){
+		for (int q = 0 ; q <= 1 ; q++){
+			for (int r = 0 ; r <=1 ; r++){
+				cout << p << " " << q << " " << r << " | ";
+				cout << !(p||q||!r) && (alsdan(r,p)||alsdan(r,q));
+				cout << endl;
+			}
+		}
+	}
+	cout << endl << "uitdrukking 2" << endl;
+	for (int p = 0 ; p <= 1 ; p++){
+		for (int q = 0 ; q <= 1 ; q++){
+			for (int r = 0 ; r <=1 ; r++){
+				cout << p << " " << q << " " << r << " | ";
+				cout << alsdan((r&&!q),(alsdan(q,r)||alsdan(p,r)));
+				cout << endl;
+			}
+		}
+	}
+	cout << endl << "uitdrukking 3" << endl;
+	for (int p = 0 ; p <= 1 ; p++){
+		for (int q = 0 ; q <= 1 ; q++){
+			for (int r = 0 ; r <=1 ; r++){
+				for (int s = 0 ; s <=1 ; s++){
+					cout << p << " " << q << " " << r << " " << s << " | ";
+					cout << alsdan(alsdan(p,alsdan(q,alsdan(r,s))),alsdan(alsdan(alsdan(p,q),r),s));
+					cout << endl;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+bool uitdrukking(const bool p,const bool q,const bool r){
+
+}
+bool alsdan(const bool p,const bool q){
+	return q || !(p||q);
+}
