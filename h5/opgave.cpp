904faed7e3b3483cf98df1839e162fa6822e3e1e
AndreasDL <andreasdelille@hotmail;com>
2014-03-10 22:17:33 +0100

    diophantische vergelijkingen lineair klaar!

--- h5/opgave.cpp
+++ h5/opgave.cpp
@@ -2,53 +2,35 @@
 #include <stack>
 #include <vector>
 #include <string>
-#include <sstream>
 using namespace std;
 
-int ggd(int a, int b,stack<vector<int> > &coef);
-void schrijfUit(stack<vector<int> > &coef);
+//http://stackoverflow.com/questions/4917003/whats-algorithm-used-to-solve-linear-diophantine-equation-ax-by-c
+int ggd(int a, int b, int &x, int &y)
+{
+    if (a % b == 0)
+    {
+        x = 0;
+        y = 1;
+        return b;
+    }
+
+    int newx, newy;
+    int ret = ggd(b, a % b, newx, newy);
+
+    x = newy;
+    y = newx - newy * (a / b);
+    return ret;
+}
 
 int main(){
 	stack<vector<int> > coef;
-	cout << ggd(316,288,coef) << " = ";
+	//cout << "ggd = " << ggd(316,288,coef) << endl;
 
-	coef.pop();
-	schrijfUit(coef);
-}
-void schrijfUit(stack<vector<int> > &coef){
-	vector<int> temp = coef.top();
-	coef.pop();
-	
-	cout << temp[0] << " - " << temp[2] << " x (";
-	if (coef.empty()){
-		cout << temp[1];
-	}else{
-		schrijfUit(coef);
-	}
-	cout << ")";
-		
-}
-int ggd(int a, int b,stack<vector<int> > &coef){
-	if (b > a){
-		return ggd(b,a,coef);
-	}else{
-		int rest;
+	int x , y , a = 316 , b = 288, c = 40;
+	int d = ggd(a,b,x,y);
+	int e = b/d, f = -a/d;
 
-		while (b != 0){
-			rest = a % b;
-			int c = a/b; //gehele deling
-			
-			// a = b*c + rest
-			vector<int> temp(4);
-			temp[0] = a;
-			temp[1] = b;
-			temp[2] = c;
-			temp[3] = rest;
-			coef.push(temp);
-
-			a = b;
-			b = rest;
-		}
-		return a;
-	}
+	cout << "oplossing van " << a << "x + " << b << "y = " << c << endl;
+	cout << "x = " << x << " + " << e << "k" << endl;
+	cout << "y = " << y << " + " << f << "k" << endl;
 }
